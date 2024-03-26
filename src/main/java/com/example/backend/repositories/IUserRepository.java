1cdebb8ca45d094697b9621515e0285bc5816fc5
jhosep <jhosephgh14@gmail.com>
2024-03-26 14:27:55 -0500

    comit crud user

---
+++ src/main/java/com/example/backend/repositories/IUserRepository.java
@@ -0,0 +1,10 @@
+package com.example.backend.repositories;
+
+import com.example.backend.models.UserModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IUserRepository extends JpaRepository<UserModel, Long> {
+
+}
